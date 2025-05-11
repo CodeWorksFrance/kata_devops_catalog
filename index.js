baa840180f7cb51ca5cf11e624f13b91237e7301
Dimitri Merejkowsky <dimitri@dmerej.info>
2025-05-11 16:07:53 +0200

    getBooks(): more realistic API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const getBooks = () => [
-    "Open Space Technology",
-    "Accelerate",
-    "The Phoenix Project"
+    {id : 1, title: "Open Space Technology"},
+    {id : 2, title: "Accelerate"},
+    {id : 3, title: "The Phoenix Project"},
 ]
 
 module.exports = { getBooks }
